ee92282bc41a4c3d7c502f2b937ee4268812f1b0
Andrey <34690892+1985IvanovAndrey@users.noreply.github.com>
2018-10-31 02:27:07 +0200

    Update InitDataToDb.java

--- src/main/java/com/base/login/spring/mvc/appConfig/InitDataToDb.java
+++ src/main/java/com/base/login/spring/mvc/appConfig/InitDataToDb.java
@@ -1,59 +1,59 @@
-// package com.base.login.spring.mvc.appConfig;//package service.bookmark.config;
+package com.base.login.spring.mvc.appConfig;//package service.bookmark.config;
 
 
-// import com.base.login.spring.mvc.dao.model.Bookmark;
-// import com.base.login.spring.mvc.dao.model.Groups;
-// import com.base.login.spring.mvc.dao.model.Role;
-// import com.base.login.spring.mvc.dao.model.UserInfo;
-// import com.base.login.spring.mvc.dao.repository.BookmarkRepository;
-// import com.base.login.spring.mvc.dao.repository.GroupsRepository;
-// import com.base.login.spring.mvc.dao.repository.RoleRepository;
-// import com.base.login.spring.mvc.dao.repository.UserRepository;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.stereotype.Component;
-// import org.springframework.transaction.annotation.Transactional;
+import com.base.login.spring.mvc.dao.model.Bookmark;
+import com.base.login.spring.mvc.dao.model.Groups;
+import com.base.login.spring.mvc.dao.model.Role;
+import com.base.login.spring.mvc.dao.model.UserInfo;
+import com.base.login.spring.mvc.dao.repository.BookmarkRepository;
+import com.base.login.spring.mvc.dao.repository.GroupsRepository;
+import com.base.login.spring.mvc.dao.repository.RoleRepository;
+import com.base.login.spring.mvc.dao.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 
-// import javax.annotation.PostConstruct;
-// import java.awt.List;
-// import java.util.*;
+import javax.annotation.PostConstruct;
+import java.awt.List;
+import java.util.*;
 
-// @Transactional
-// @Component
-// public class InitDataToDb {
+@Transactional
+@Component
+public class InitDataToDb {
 
-//     @Autowired
-//     private UserRepository userRepository;
-//     @Autowired
-//     private BCryptPasswordEncoder bCryptPasswordEncoder;
-//     @Autowired
-//     private RoleRepository roleRepository;
-//     @Autowired
-//     private BookmarkRepository bookmarkRepository;
-//     @Autowired
-//     private GroupsRepository groupsRepository;
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    @Autowired
+    private RoleRepository roleRepository;
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+    @Autowired
+    private GroupsRepository groupsRepository;
 
-//     @PostConstruct
-//     private void init() {
+    @PostConstruct
+    private void init() {
 
-//         Role role1 = new Role();
-//         role1.setId(1L);
-//         role1.setName("admin");
-//         roleRepository.save(role1);
+        Role role1 = new Role();
+        role1.setId(1L);
+        role1.setName("admin");
+        roleRepository.save(role1);
 
-//         Role role2 = new Role();
-//         role2.setId(2L);
-//         role2.setName("user");
-//         roleRepository.save(role2);
+        Role role2 = new Role();
+        role2.setId(2L);
+        role2.setName("user");
+        roleRepository.save(role2);
 
 
-//         UserInfo userInfo = new UserInfo();
-//         userInfo.setUsername("boss@gmail.com");
-//         userInfo.setPassword(bCryptPasswordEncoder.encode("12345678"));
-//         Set<Role> roles = new HashSet<>(Collections.singleton(role1));
-//         userInfo.setRoles(roles);
-//         userRepository.save(userInfo);
+        UserInfo userInfo = new UserInfo();
+        userInfo.setUsername("boss@gmail.com");
+        userInfo.setPassword(bCryptPasswordEncoder.encode("12345678"));
+        Set<Role> roles = new HashSet<>(Collections.singleton(role1));
+        userInfo.setRoles(roles);
+        userRepository.save(userInfo);
 
 // //        Groups group=new Groups();
 // //        Bookmark bookmark=new Bookmark();
