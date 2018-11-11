009aa22ba9a457bb2267faa3505c5caf94a265cc
1985IvanovAndrey <dron_krotova@i.ua>
2018-11-11 15:32:47 +0200

    update for heroku

--- src/main/java/com/base/login/spring/mvc/web/UserController.java
+++ src/main/java/com/base/login/spring/mvc/web/UserController.java
@@ -90,7 +90,6 @@ public class UserController {
 
         if (logout != null)
             model.addAttribute("message", "You have been logged out successfully.");
-
         return "login1";
     }
 
